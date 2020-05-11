60e19fff2cf0f631774bc9b8e3325fb70ac0c223
Antonio Rivero Ostoic <multiplex@post.com>
2020-05-11 10:58:30 +0200

    Update get.edh

--- R/get.edh.R
+++ R/get.edh.R
@@ -1,21 +1,42 @@
+## 
+## FUNCTION get.edh() to get data API from the Epigraphic Database Heidelber with R
+## (CC BY-SA 4.0) Antonio Rivero Ostoic, jaro@cas.au.dk 
+##
+## version 0.2.1 (27-04-2020)
+##
+## Parameter description from https://edh-www.adw.uni-heidelberg.de/data/api
+##
+## Search parameters for inscriptions and geography:
+## province (get list of valid values at https://edh-www.adw.uni-heidelberg.de/data/api/terms/province, case insensitive)
+## country (get list of valid values at https://edh-www.adw.uni-heidelberg.de/data/api/terms/country, case insensitive)
+## findspot_modern (add leading and/or trailing truncation by asterisk *, e.g. findspot_modern=köln*, case insensitive)
+## findspot_ancient (add leading and/or trailing truncation by asterisk *, e.g. findspot_ancient=aquae*, case insensitive)
+## bbox (bounding box in the format bbox=minLong , minLat , maxLong , maxLat , example: https://edh-www.adw.uni-heidelberg.de/data/api/inscriptions/search?bbox=11,47,12,48)
+##
+## Search parameters for inscriptions:
+## hd_nr (HD-No of inscription)
+## year_not_before (integer, BC years are negative integers)
+## year_not_after (integer, BC years are negative integers)
+## tm_nr (integer value)
+## transcription (automatic leading & trailing truncation, brackets are ignored)
+## type (of inscription, get list of values at https://edh-www.adw.uni-heidelberg.de/data/api/terms/type, case insensitive)
+##
+## Search parameters for geography:
+## findspot (level of village, street etc.; add leading and/or trailing truncation by asterisk *, e.g. findspot_modern=köln*, case insensitive)
+## pleiades_id (Pleiades identifier of a place; integer value)
+## geonames_id (Geonames identifier of a place; integer value)
+## 
+## Additional parameters:
+## search (whether to search in "inscriptions" or in "geography")
+## addID (whether or not add numeric ID to the list)
+## printQ (print query?)
+
 get.edh <-
 function (search = c("inscriptions", "geography"), url = "https://edh-www.adw.uni-heidelberg.de/data/api", 
     hd_nr, province, country, findspot_modern, findspot_ancient, 
     year_not_before, year_not_after, tm_nr, transcription, type, 
-    bbox, findspot, pleiades_id, geonames_id, offset, limit, 
-    addID, printQ) 
+    bbox, findspot, pleiades_id, geonames_id, addID, printQ) 
 {
-    ifelse(missing(hd_nr) == FALSE, NA, hd_nr <- "")
-    ifelse(missing(province) == FALSE, NA, province <- "")
-    ifelse(missing(country) == FALSE, NA, country <- "")
-    ifelse(missing(findspot_modern) == FALSE, NA, findspot_modern <- "")
-    ifelse(missing(findspot_ancient) == FALSE, NA, findspot_ancient <- "")
-    ifelse(missing(year_not_before) == FALSE, NA, year_not_before <- "")
-    ifelse(missing(year_not_after) == FALSE, NA, year_not_after <- "")
-    ifelse(missing(tm_nr) == FALSE, NA, tm_nr <- "")
-    ifelse(missing(transcription) == FALSE, NA, transcription <- "")
-    ifelse(missing(type) == FALSE, NA, type <- "")
-    ifelse(missing(bbox) == FALSE, NA, bbox <- "")
     ifelse(missing(hd_nr) == FALSE, NA, hd_nr <- "")
     ifelse(missing(province) == FALSE, NA, province <- "")
     ifelse(missing(country) == FALSE, NA, country <- "")
@@ -30,8 +51,6 @@ function (search = c("inscriptions", "geography"), url = "https://edh-www.adw.un
     ifelse(missing(findspot) == FALSE, NA, findspot <- "")
     ifelse(missing(pleiades_id) == FALSE, NA, pleiades_id <- "")
     ifelse(missing(geonames_id) == FALSE, NA, geonames_id <- "")
-    ifelse(missing(offset) == FALSE, NA, offset <- "")
-    ifelse(missing(limit) == FALSE, NA, limit <- "")
     ifelse(missing(addID) == FALSE && isTRUE(addID == TRUE) == 
         FALSE, addID <- FALSE, addID <- TRUE)
     ifelse(missing(printQ) == FALSE && isTRUE(printQ == TRUE) == 
@@ -44,8 +63,7 @@ function (search = c("inscriptions", "geography"), url = "https://edh-www.adw.un
             "&", "year_not_before=", year_not_before, "&", "year_not_after=", 
             year_not_after, "&", "tm_nr=", tm_nr, "&", "transcription=", 
             transcription, "&", "type=", type, "&", "bbox=", 
-            bbox, "&", "offset=", offset, "&", "limit=", limit, 
-            sep = "")
+            bbox, sep = "")
     }
     else if (match.arg(search) == "geography") {
         URL <- paste(url, match.arg(search), "search?", sep = "/")
@@ -69,20 +87,11 @@ function (search = c("inscriptions", "geography"), url = "https://edh-www.adw.un
         nstring <- paste0(strsplit(sst[1], "[?]")[[1]][1], "?", 
             sep = "")
     }
-    if (any(sst == "limit=") == TRUE) {
-        total <- rjson::fromJSON(file = sub("=$", "=1", string))$total
-        sst[which(sst == "limit=")] <- paste0("limit=", total, 
-            sep = "")
-        rm(total)
-    }
-    else {
-        NA
-    }
     for (i in seq(2, length(sst))) {
         if (isTRUE(substr(sst[i], nchar(sst[i]), nchar(sst[i])) == 
             "=") == FALSE && isTRUE(substr(sst[i], nchar(sst[i]) - 
             5, nchar(sst[i])) == "%22%22") == FALSE) {
-            nstring <- paste0(nstring, sst[i], sep = "&")
+            nstring <- paste0(nstring, sst[i], sep = "")
         }
         else {
             NA
@@ -90,9 +99,9 @@ function (search = c("inscriptions", "geography"), url = "https://edh-www.adw.un
     }
     rm(i)
     if (isTRUE(printQ == TRUE) == TRUE) {
-        print(sub("&$", "", nstring))
+        print(nstring)
     }
-    raw.dat <- rjson::fromJSON(file = sub("&$", "", nstring))
+    raw.dat <- rjson::fromJSON(file = nstring)
     dat <- raw.dat$items
     if (isTRUE((raw.dat$total) == 0L) == TRUE || isTRUE(length(raw.dat) == 
         0L) == TRUE) {
